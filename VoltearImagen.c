f21faed2ca9a8aef2453e06b55d146ff5bc0250b
Juan Meza <diegopaz04717@gmail.com>
2026-04-30 21:56:53 -0500

    Se arregla el algoritmo en c

--- VoltearImagen.c
+++ VoltearImagen.c
@@ -3,14 +3,23 @@
 #define FILAS 5
 #define COLAS 5
 
-int LlenarImagen(int imagen[FILAS][COLAS]);
+void llenarImagen(int imagen[FILAS][COLAS]);
 void imagenEspejo(int imagen[FILAS][COLAS]);
 void imprimir(int imagen[FILAS][COLAS]);
 int main(int argc, char *argv[]) {
-	imagenEspejo();
+	int imagen[FILAS][COLAS];
+	
+	llenarImagen(imagen);
+	printf("Original:\n");
+	imprimir(imagen);
+	
+	imagenEspejo(imagen);
+	printf("\nEspejo:\n");
+	imprimir(imagen);
+	
 	return 0;
 }
-void LlenarImagen(int imagen[FILAS][COLAS]){
+void llenarImagen(int imagen[FILAS][COLAS]){
 	int img[FILAS][COLAS] = {
 		{0,1,0,0,0},
 		{0,1,1,0,0},
@@ -27,7 +36,7 @@ void LlenarImagen(int imagen[FILAS][COLAS]){
 void imprimir(int imagen[FILAS][COLAS]){
 	for(int i=0;i<FILAS;i++){
 		for(int j=0;j<COLAS;j++){
-			(" %d ",imagen[i][j]);
+			printf(" %d ", imagen[i][j]);
 		}
 		printf("\n");
 	}
@@ -38,9 +47,9 @@ void imagenEspejo(int imagen[FILAS][COLAS]){
 		int der = COLAS - 1;
 		
 		while(izq < der) {
-			int temp = img[i][izq];
-			img[i][izq] = img[i][der];
-			img[i][der] = temp;
+			int temp = imagen[i][izq];
+			imagen[i][izq] = imagen[i][der];
+			imagen[i][der] = temp;
 			
 			izq++;
 			der--;
